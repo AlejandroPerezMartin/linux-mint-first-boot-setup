6322fe6eecb58253186f0d2b459b68123defc7a3
Alejandro <aleperez92@gmail.com>
2014-06-05 23:42:44 +0100

    Added Git config

--- lmint-first-boot-setup.sh
+++ lmint-first-boot-setup.sh
@@ -10,13 +10,15 @@
 sudo apt-get update
 sudo apt-get -qy dist-upgrade
 
+
 ##### Applications to be removed #####
-appsToRemove=( tomboy thunderbird* simple-scan gthumb pidgin xchat* libreoffice-calc libreoffice-base brasero* banshee vlc* transmission* )
+appsToRemove=( tomboy hexchat thunderbird* simple-scan gthumb pidgin xchat* libreoffice-calc libreoffice-impress libreoffice-draw libreoffice-math libreoffice-base brasero* banshee vlc* transmission* )
 
 for app in "${appsToRemove[@]}"
 do
     sudo apt-get purge -qy $app
 done
+
 
 ##### Add repositories #####
 echo "deb http://repository.spotify.com stable non-free" | sudo tee -a /etc/apt/sources.list
@@ -26,6 +28,7 @@ sudo add-apt-repository -y ppa:numix/ppa
 sudo add-apt-repository -y ppa:ricotz/docky
 sudo apt-get update
 
+
 ##### Install software #####
 appsToInstall=( deluge sublime-text git filezilla plank vimix-flat-themes spotify-client bleachbit netbeans gufw trimage imagemagick nodejs* npm numix-theme numix-plank-theme libc6-i386 lib32stdc++6 lib32gcc1 lib32ncurses5 ia32-libs icnsutils )
 
@@ -33,6 +36,12 @@ for app in "${appsToInstall[@]}"
 do
     sudo apt-get install -qy $app
 done
+
+
+##### Configure Git #####
+git config --global user.name "Alejandro"
+git config --global user.email "aleperez92@gmail.com"
+
 
 ##### Install Flattr icons #####
 sudo git clone "https://github.com/NitruxSA/flattr-icons.git" /usr/share/icons/flattr-icons
@@ -44,16 +53,18 @@ sudo tar -xf eOS.tar.gz
 sudo rm -f eOS.tar.gz
 cd ~
 
+
 ##### Install Source Code Pro font #####
-wget -c "http://downloads.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip"
-wget -c "https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf"
 mkdir ~/.fonts
+wget -c "http://downloads.sourceforge.net/project/sourcecodepro.adobe/SourceCodePro_FontsOnly-1.017.zip"
+wget -c "https://github.com/cstrap/monaco-font/raw/master/Monaco_Linux.ttf" -P ~/.fonts
 unzip SourceCode*.zip -d ~/.fonts
-mv *.ttf ~/.fonts
 sudo rm -f SourceCode*.zip
+
 
 # Update font cache
 sudo fc-cache -f -v
+
 
 ##### Install Google Chrome 32/64 bits #####
 if [ $(getconf LONG_BIT) -eq 32 ]
@@ -66,6 +77,7 @@ fi
 sudo dpkg --install *chrome*.deb
 sudo rm -f *chrome*.deb
 
+
 ##### Install Android Developer Tools 32/64 bits #####
 if [ $(getconf LONG_BIT) -eq 32 ]
 then
@@ -77,6 +89,7 @@ fi
 unzip adt*.zip -d ~
 sudo rm -f adt*.zip
 
+
 ##### Install Komodo IDE 32/64 bits #####
 if [ $(getconf LONG_BIT) -eq 32 ]
 then
@@ -85,35 +98,41 @@ else
     wget -c "http://downloads.activestate.com/Komodo/releases/8.5.3/Komodo-Edit-8.5.3-14067-linux-x86_64.tar.gz"
 fi
 
-sudo mkdir /opt/komodo-ide
 tar -xf Komodo*.tar.gz
 cd Komodo*
-echo "DIRECTORY IS: /opt/komodo-ide"
-sudo sh install.sh
+sudo mkdir /opt/komodo-ide
+echo "DIRECTORY: /opt/komodo-ide"
+sudo ./install.sh
+[[ -e "/opt/komodo-ide/bin/komodo" ]] && sudo ln -s /opt/komodo-ide/bin/komodo /usr/bin/komodo
 cd ..
 sudo rm -f Komodo*.zip
 
+
 ##### Fix broken dependencies #####
 sudo apt-get -qfy install
+
 
 ##### Clean #####
 sudo apt-get -y autoremove
 sudo apt-get -y autoclean
 
+
 ##### Change settings #####
-gsettings set org.cinnamon desktop-layout "flipped"
-gsettings set org.cinnamon.desktop.interface gtk-theme "Vimix"
-gsettings set org.cinnamon.desktop.wm.preferences theme "Vimix-Dark"
-gsettings set org.cinnamon.desktop.interface cursor-theme "Adwaita"
-gsettings set org.cinnamon.desktop.interface icon-theme "flattr-icons"
-gsettings set org.cinnamon favorite-apps "['firefox.desktop', 'google-chrome.desktop', 'sublime_text.desktop', 'gnome-terminal.desktop', 'nemo.desktop']"
-gsettings set org.nemo.desktop volumes-visible true
-gsettings set org.nemo.desktop home-icon-visible true
-gsettings set org.nemo.desktop trash-icon-visible true
-gsettings set org.nemo.desktop computer-icon-visible false
+dbus-launch gsettings set org.cinnamon desktop-layout "flipped"
+dbus-launch gsettings set org.cinnamon.desktop.interface gtk-theme "Vimix"
+dbus-launch gsettings set org.cinnamon.desktop.wm.preferences theme "Vimix-Dark"
+dbus-launch gsettings set org.cinnamon.desktop.interface cursor-theme "Adwaita"
+dbus-launch gsettings set org.cinnamon.desktop.interface icon-theme "flattr-icons"
+dbus-launch gsettings set org.cinnamon favorite-apps "['firefox.desktop', 'google-chrome.desktop', 'sublime_text.desktop', 'gnome-terminal.desktop', 'nemo.desktop']"
+dbus-launch gsettings set org.nemo.desktop volumes-visible true
+dbus-launch gsettings set org.nemo.desktop home-icon-visible true
+dbus-launch gsettings set org.nemo.desktop trash-icon-visible true
+dbus-launch gsettings set org.nemo.desktop computer-icon-visible false
+
 
 ##### Restart Cinnamon #####
 cinnamon --replace
+
 
 ##### Exit script #####
 exit
